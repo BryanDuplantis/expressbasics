d76b12a1e26aac5bb96311f3aad5b93aec0ac843
Bryan Duplantis <duplantis@gmail.com>
2015-08-05 10:11:08 -0500

    added query parameter to pizza.js

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var chickennuggets = require('./routes/chickennuggets');
 
 var app = express();
 
+require('./lib/secrets');
+
 app.set('view engine', 'ejs');
 app.set('case sensitive routing', true);
 
